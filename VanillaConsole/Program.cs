75d40f6a9d956ae90cc249b5811d21618e4fc198
jayongg <jayongg@microsoft.com>
2017-02-08 07:55:02 -0800

    checkin

--- VanillaConsole/Program.cs
+++ VanillaConsole/Program.cs
@@ -16,54 +16,51 @@ namespace VanillaConsole
             var app = new Application();
 
             // get the hierarchy
-            string xmlHierarchy;
-            app.GetHierarchy(null, HierarchyScope.hsPages, out xmlHierarchy);
+            //string xmlHierarchy;
+            //app.GetHierarchy(null, HierarchyScope.hsPages, out xmlHierarchy);
 
-            Console.WriteLine("Hierarchy:\n" + xmlHierarchy);
+            //Console.WriteLine("Hierarchy:\n" + xmlHierarchy);
 
-            // now find the current page, print out its ID
-            var xdoc = XDocument.Parse(xmlHierarchy);
-            var ns = xdoc.Root.Name.Namespace;
+            //// now find the current page, print out its ID
+            //var xdoc = XDocument.Parse(xmlHierarchy);
+            //var ns = xdoc.Root.Name.Namespace;
 
             var pageId = app.Windows.CurrentWindow.CurrentPageId;
             Console.WriteLine("Current Page ID: " + pageId);
 
+            string xmlPage;
+            //app.GetPageContent(pageId, out xmlPage);
+
             //Export page as a section
-            app.Publish(pageId, "f:\\projects", 0);
+            //app.Publish(pageId, "f:\\projects", 0);
 
             //get notebook id
             var notebookId = app.Windows.CurrentWindow.CurrentNotebookId;
 
-            //add page to notebook
-            string pbstrObjectID;
-            app.OpenHierarchy("f:\\projects", notebookId, out pbstrObjectID);
+            var sectionId = app.Windows.CurrentWindow.CurrentSectionId;
 
-            //get destination section id
-
-            //Merge two sections
-            app.MergeSections(pbstrObjectID, pbstrObjectID);
-
-
-            // get the page content, print it out
-            string xmlPage;
+            //string pageId;
+            app.CreateNewPage(sectionId, out pageId);
             app.GetPageContent(pageId, out xmlPage);
-            Console.WriteLine("Page XML:\n" + xmlPage);
-
-            // sample - this is how to update content - normally you would modify the xml.
-            app.UpdatePageContent(xmlPage);
-
-            // bonus - if there are any images, get the binary content of the first one
             var xPage = XDocument.Parse(xmlPage);
-            var xImage = xPage.Descendants(ns + "Image").FirstOrDefault();
-            if (xImage != null)
-            {
-                var xImageCallbackID = xImage.Elements(ns + "CallbackID").First();
-                var imageId = xImageCallbackID.Attribute("callbackID").Value;
-                string base64Out;
-                app.GetBinaryPageContent(pageId, imageId, out base64Out);
+            var ns = xPage.Root.Name.Namespace;
 
-                Console.WriteLine("Image found, base64 data is:\n" + base64Out);
-            }
+            var inXml = @"<one:Outline  xmlns:one=""http://schemas.microsoft.com/office/onenote/2013/onenote"">" +
+            @"<one:OEChildren>" +
+            @"<one:OE>" +
+                @"<one:T>" +
+                    @"https://www.youtube.com/watch?v=ezosSMzPdNQ" +
+                @"</one:T>" +
+            @"</one:OE>" +
+        @"</one:OEChildren>" +
+    @"</one:Outline>" +
+@"";
+            var xElement = XElement.Parse(inXml);
+            ((XElement)xPage.FirstNode).Add(xElement);
+
+            var xmlString = xPage.ToString();
+            app.UpdatePageContent(xmlString);
+
         }
     }
 }
