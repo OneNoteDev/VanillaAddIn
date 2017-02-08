ff1b8de7e1bb225814fb789e4d278e2ac89c574e
jayongg <jayongg@microsoft.com>
2017-02-08 07:56:23 -0800

    push

--- VanillaConsole/Program.cs
+++ VanillaConsole/Program.cs
@@ -15,46 +15,27 @@ namespace VanillaConsole
             // skipping error checking, just demonstrating using these APIs
             var app = new Application();
 
-            // get the hierarchy
-            //string xmlHierarchy;
-            //app.GetHierarchy(null, HierarchyScope.hsPages, out xmlHierarchy);
-
-            //Console.WriteLine("Hierarchy:\n" + xmlHierarchy);
-
-            //// now find the current page, print out its ID
-            //var xdoc = XDocument.Parse(xmlHierarchy);
-            //var ns = xdoc.Root.Name.Namespace;
-
             var pageId = app.Windows.CurrentWindow.CurrentPageId;
             Console.WriteLine("Current Page ID: " + pageId);
 
             string xmlPage;
-            //app.GetPageContent(pageId, out xmlPage);
-
-            //Export page as a section
-            //app.Publish(pageId, "f:\\projects", 0);
-
-            //get notebook id
-            var notebookId = app.Windows.CurrentWindow.CurrentNotebookId;
-
             var sectionId = app.Windows.CurrentWindow.CurrentSectionId;
 
-            //string pageId;
             app.CreateNewPage(sectionId, out pageId);
             app.GetPageContent(pageId, out xmlPage);
             var xPage = XDocument.Parse(xmlPage);
             var ns = xPage.Root.Name.Namespace;
 
             var inXml = @"<one:Outline  xmlns:one=""http://schemas.microsoft.com/office/onenote/2013/onenote"">" +
-            @"<one:OEChildren>" +
-            @"<one:OE>" +
-                @"<one:T>" +
-                    @"https://www.youtube.com/watch?v=ezosSMzPdNQ" +
-                @"</one:T>" +
-            @"</one:OE>" +
-        @"</one:OEChildren>" +
-    @"</one:Outline>" +
-@"";
+                                    @"<one:OEChildren>" +
+                                    @"<one:OE>" +
+                                        @"<one:T>" +
+                                            @"https://www.youtube.com/watch?v=ezosSMzPdNQ" +
+                                        @"</one:T>" +
+                                    @"</one:OE>" +
+                                @"</one:OEChildren>" +
+                            @"</one:Outline>" +
+                        @"";
             var xElement = XElement.Parse(inXml);
             ((XElement)xPage.FirstNode).Add(xElement);
 
